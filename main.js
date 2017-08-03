ee7f01e5c68016305746a6b5c6a6a8c62b8ea284
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-08-03 16:11:38 -0600

    continued ajax work

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
 $(document).ready(function (){
 
     // DATA from 7-27-2017 through 8-02-2017 (Asteroids Close To Earth);
-    $.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-07-27&end_date=2017-08-02&detailed=true&api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9').then(function (data){
-        console.log(data);
-    });
-
-    // ASTEROID ONE:
-
-    $.get('https://api.nasa.gov/neo/rest/v1/neo/3772382?api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9').then(function (dataA1){
-        console.log(dataA1);
-    });
+    // $.get('ttps://api.nasa.gov/neo/rest/v1/feed?start_date=2017-08-01&end_date=2017-08-01&detailed=true&api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9').then(function (data){
+    //     console.log(data);
+    // });
 
     $.ajax({
         method:'GET',
-        url: 'https://api.nasa.gov/neo/rest/v1/neo/3772382?api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9',
-        success: function(result) {
-            $('#asteroidData').append('<td>' + result.name + '</td>');
-            $('#asteroidData').append('<td>' + result.estimated_diameter.feet.estimated_diameter_min + " (Feet)" + '</td>');
-            $('#asteroidData').append('<td>' + result.close_approach_data[9].miss_distance.miles + " (Miles)" + '</td>');
-            $('#asteroidData').append('<td>' + result.close_approach_data[9].relative_velocity.miles_per_hour + " (MPH)" + '</td>');
+        url: 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-08-01&end_date=2017-08-01&detailed=true&api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9',
+        success: function(data) {
+                // console.log(JSON.stringify(result));
+            var nearEarth = data.near_earth_objects;
+            var newEarthDate = (nearEarth['2017-08-01']);
+
+            var trueDanger = [];
+            for(var key in newEarthDate){
+                if (newEarthDate[key].is_potentially_hazardous_asteroid){
+                    trueDanger.push(newEarthDate[key]);
+                }
+            } console.log(trueDanger);
+
+            // how can I dinamically populate my dom using jquery.
+
+
+
+            for (var key in nearEarth){
+                // console.log(nearEarth[key]['0'].is_potentially_hazardous_asteroid);
+                // console.log(nearEarth['2017-08-01']['0'].is_potentially_hazardous_asteroid);
+            }
+
         }
     });
+
+
+    // ASTEROID ONE:
+
+    // $.get('ttps://api.nasa.gov/neo/rest/v1/neo/3772382?api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9').then(function (dataA1){
+    //     console.log(dataA1);
+    // });
+
+    // $.ajax({
+    //     method:'GET',
+        // url: 'ttps://api.nasa.gov/neo/rest/v1/neo/3772382?api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9',
+    //     success: function(result) {
+
+            // $('#asteroidData').append('<td>' + result.name + '</td>');
+            // $('#asteroidData').append('<td>' + result.estimated_diameter.feet.estimated_diameter_min + " feet wide" + '</td>');
+            // $('#asteroidData').append('<td>' + result.close_approach_data[9].miss_distance.miles + " miles away" + '</td>');
+            // $('#asteroidData').append('<td>' + result.close_approach_data[9].relative_velocity.miles_per_hour + " mph" + '</td>');
+    //     }
+    // });
 
     // ASTEROID TWO:
 
