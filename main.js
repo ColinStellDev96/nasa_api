aeedf259b364b56c87fda70f28bab16dae6c9d69
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-08-04 18:45:12 -0600

    one method that works, work begin on larger scope

--- main.js
+++ main.js
@@ -7,66 +7,47 @@ $(document).ready(function (){
 
     $.ajax({
         method:'GET',
-        url: 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-08-01&end_date=2017-08-01&detailed=true&api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9',
+        url: 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-07-27&end_date=2017-08-02&detailed=true&api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9',
         success: function(data) {
-                // console.log(JSON.stringify(result));
-
+            console.log(data);
             var nearEarth = data.near_earth_objects;
-            var newEarthDate = (nearEarth['2017-08-01']);
 
-            var trueDanger = []; /*Asteroids With Potential Hazard True */
-            for(var key in newEarthDate){
-                if (newEarthDate[key].is_potentially_hazardous_asteroid){
-                    trueDanger.push(newEarthDate[key]);
-                }
+            var trueDanger = [];
+            for (var property in nearEarth) {
+                if (nearEarth[property].is_potentially_hazardous_asteroid === true)
+                trueDanger.push(nearEarth[key]);
             } console.log(trueDanger);
-
-            var nameData = []; /* Pulling Asteroid Names */
-            for (var key in trueDanger){
-                nameData.push(trueDanger[key].name);
-            } console.log(name);
-            var asteroidName = '<td>' + nameData + '</td>';
-            console.log(asteroidName);
-
-
-
-
-
-            // how can I dinamically populate my dom using jquery.
-
-
-
-            for (var key in nearEarth){
-                // console.log(nearEarth[key]['0'].is_potentially_hazardous_asteroid);
-                // console.log(nearEarth['2017-08-01']['0'].is_potentially_hazardous_asteroid);
-            }
-
         }
     });
 
 
-    // ASTEROID ONE:
-
-    // $.get('ttps://api.nasa.gov/neo/rest/v1/neo/3772382?api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9').then(function (dataA1){
-    //     console.log(dataA1);
-    // });
 
     // $.ajax({
     //     method:'GET',
-        // url: 'ttps://api.nasa.gov/neo/rest/v1/neo/3772382?api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9',
-    //     success: function(result) {
+    //     url: 'ttps://api.nasa.gov/neo/rest/v1/feed?start_date=2017-07-31&end_date=2017-07-31&detailed=true&api_key=S3PmfcdtXaoYuUfgeotZnnBPIM0aR73Tvc8h6Ae9',
+    //     success: function(data) {
+    //             // console.log(JSON.stringify(result));
+    //
+    //         var nearEarth = data.near_earth_objects;
+    //         var newEarthDate = (nearEarth['2017-07-31']);
+    //
+    //         var trueDanger = []; /*Asteroids With Potential Hazard True */
+    //         for(var key in newEarthDate){
+    //             if (newEarthDate[key].is_potentially_hazardous_asteroid){
+    //                 trueDanger.push(newEarthDate[key]);
+    //             }
+    //         } console.log(trueDanger);
+    //
+    //
+    //         for (let i=0; i < trueDanger.length; i++) {
+    //             $('#asteroidData').append(`
+    //                 <td>${trueDanger[i].name}</td>
+    //                 <td>${trueDanger[i].estimated_diameter.feet.estimated_diameter_max}</td>
+    //                 <td>${trueDanger[i].close_approach_data[0].miss_distance.miles}
+    //                 <td>${trueDanger[i].close_approach_data[0].relative_velocity.miles_per_hour}</td>
+    //             `);
+    //         }
 
-            // $('#asteroidData').append('<td>' + result.name + '</td>');
-            // $('#asteroidData').append('<td>' + result.estimated_diameter.feet.estimated_diameter_min + " feet wide" + '</td>');
-            // $('#asteroidData').append('<td>' + result.close_approach_data[9].miss_distance.miles + " miles away" + '</td>');
-            // $('#asteroidData').append('<td>' + result.close_approach_data[9].relative_velocity.miles_per_hour + " mph" + '</td>');
-    //     }
-    // });
-
-    // ASTEROID TWO:
 
 
-    // ASTEROID THREE:
-
-
-});
+        });
